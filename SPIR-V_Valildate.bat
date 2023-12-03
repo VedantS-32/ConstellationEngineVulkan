ab0e399935a05475ba481ff590ad1f7a83519488
VedantS <122535961+FuzzyFox-32@users.noreply.github.com>
2023-12-04 00:19:51 +0530

    fixed spirv-val script

--- SPIR-V_Valildate.bat
+++ SPIR-V_Valildate.bat
@@ -1,3 +1,3 @@
-C:\VulkanSDK\1.3.268.0\Bin\spirv-val.exe -o ConstellationEngineCore\shader\vert.val ConstellationEngineCore\shader\vert.spv
-C:\VulkanSDK\1.3.268.0\Bin\spirv-val.exe -o ConstellationEngineCore\shader\frag.val ConstellationEngineCore\shader\frag.spv
+C:\VulkanSDK\1.3.268.0\Bin\spirv-val.exe ConstellationEngineCore\shader\vert.spv
+C:\VulkanSDK\1.3.268.0\Bin\spirv-val.exe ConstellationEngineCore\shader\frag.spv
 pause
